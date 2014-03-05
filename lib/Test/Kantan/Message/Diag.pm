0a877c1d9606c4dc0bc660a3810a012acb26a1c3
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-03-05 16:39:27 +0900

    Dump if the diag message is the refeference.

--- lib/Test/Kantan/Message/Diag.pm
+++ lib/Test/Kantan/Message/Diag.pm
@@ -13,7 +13,15 @@ sub as_string {
     my ($self, %args) = @_;
     my $reporter = $args{reporter} or die;
 
-    my $msg = $self->message // '(undef)';
+    my $msg = do {
+        if (not defined $self->message) {
+            '(undef)';
+        } elsif (ref $self->message) {
+            $reporter->dump_data($self->message);
+        } else {
+            $self->message;
+        }
+    };
     $msg =~ s/\n/\\n/g;
     return sprintf("Diag: %s\n  at %s line %s.\n", $reporter->colored(['magenta on_black'], $reporter->truncstr($msg, $self->cutoff)), $self->caller->filename, $self->caller->line);
 }
